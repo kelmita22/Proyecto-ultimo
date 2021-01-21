f6e422efbdadf864e041e2aff7e2d13ba03a12cf
kelmita22 <75831384+kelmita22@users.noreply.github.com>
2021-01-21 13:53:33 -0500

    actualizacion

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,11 @@
-const cipher = {
+const cipher = 
+    {
     encode:function(cifrar){console.log(cifrar)},
     decode:function(descifrar){console.log (descifrar)}
     };
 
+    
+    
 
 
 export default cipher;
