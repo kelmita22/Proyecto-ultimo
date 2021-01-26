793a0399740b6796c5bb8c7de6a118928914ff44
kelly <kelmita22@gmail.com>
2021-01-25 19:13:08 -0500

    cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,7 @@
-const cipher = 
+const cipher =
     {
     encode:function(cifrar){console.log(cifrar)},
     decode:function(descifrar){console.log (descifrar)}
     };
+
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,57 @@
 import cipher from './cipher.js';
-//Llamada a mis botones de cifrado y descifrado para activar los resultados
+//Llamada a mis botones de cifrado y descifrado para activar los resultados / Los cambié de ser eventos onclick.
 let btnCifrar = document.getElementById('encodeText')
 let btnDescifrar = document.getElementById('decodeText')
 
-console.log(cipher);
-// Mi alfabeto en Mayusculas sera la constante en el proyecto para que el resultado salga en mayuscula.
-const alphabet = ['A','B','C','D','E','F',
-                  'G','H','I','J','K','L',
-                  'M','N','O','P','Q','R',
-                  'S','T','U','V','W','X',
-                  'Y','Z'];
 // funcion para validar los desplazamientos y el ingreso de texto
-const encodeText=()=> {
-    
-    const form = document.Cifrar;
-    let sourceText = form.sourceText.value;
-    let des_cif = Number(form.des_cif.value);
-    //Texto ingresado en el textarea y resultado por el numero de desplazamiento elegido
-    cipher.encode(sourceText)
-
-    des_cif = (alphabet.length +des_cif) % alphabet.length;
-
-    form.sourceText.value = [... sourceText ].map(texto =>
-    cifrar(texto,des_cif)).join('');
-  }
-    //Click en boton para cifrar el texto ingresado en el textarea
-    btnCifrar.addEventListener('click', encodeText)
-
-// funcion para cifrar
-function cifrar(texto, des_cif) {
-  //Mi alfabeto el mayusucal
-  let include = alphabet.includes( texto .toUpperCase());
- 
-  if (include){
-   let position = alphabet.indexOf( texto.toUpperCase());
-   let newPosition = (position +des_cif) % alphabet.length;
-
-   return alphabet[newPosition];
- }
- else
- return texto;
+function encodeText() {
+  const form = document.Cifrar;
+  let sourceText = form.sourceText.value;
+  let des_cif = Number(form.des_cif.value);
+  //Texto ingresado en el textarea y resultado por el numero de desplazamiento elegido
+  form.sourceText.value = [...sourceText].map(texto => cifrar(texto, des_cif)).join('');
+  //Argumento
+  cipher.encode(sourceText);
 }
-// funcion para descifrar
-const decodeText=()=> {
+  //Click en boton para cifrar el texto ingresado en el textarea
+  btnCifrar.addEventListener('click', encodeText);
+
+  function cifrar(texto, des_cif) {
+    let newText = "";
+    for (let i = 0; i < texto.length; i++){
+    if (texto.charCodeAt(i) >= 33 && texto.charCodeAt(i) <= 90){
+        newText += String.fromCharCode(((texto.charCodeAt(i) - 65 + Number(des_cif)) % 26) + 65);
+      } else if (texto.charCodeAt(i) >= 97 &&  texto.charCodeAt(i) <= 122) {
+        newText += String.fromCharCode(((texto.charCodeAt(i) - 97 + Number(des_cif)) % 26) + 97);
+      } else if (texto.charCodeAt(i===32)) {
+        newText += "";
+      }
+      }
+      return newText;
+      };
+  
+  // funcion para descifrar
+  function decodeText() {
     const form = document.Descifrar;
-    let des_des = Number(form.des_des.value);
     let resultText = form.ResultText.value;
-    cipher.decode (resultText)
-
-    des_des = (alphabet.length -des_des) % alphabet.length;
-
-    form.ResultText.value = [... resultText ].map(texto =>
-    cifrar(texto,des_des)).join('');
+    let des_des = Number(form.des_des.value);
+    
+    form.ResultText.value = [...resultText].map(texto2 => descifrar(texto2, des_des)).join('');
+    cipher.decode(resultText);
   }
   btnDescifrar.addEventListener('click', decodeText)
 
+  function descifrar(texto2, des_des){
+    let desText = "";
+    for (let i = 0; i < texto2.length; i++){
 
-
+       if (texto2.charCodeAt(i) >= 33 && texto2.charCodeAt(i) <= 90){
+            desText += String.fromCharCode(((texto2.charCodeAt(i) - 90 - parseInt(des_des)) % 26) + 90);
+        } else if (texto2.charCodeAt(i) >= 97 &&  texto2.charCodeAt(i) <= 122) {
+            desText += String.fromCharCode(((texto2.charCodeAt(i) - 122 - parseInt(des_des)) % 26) + 122);
+        } else if (texto2.charCodeAt(i===32)) {
+          desText += " ";
+        }
+    }
+    return desText;
+};
