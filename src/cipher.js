c802ae828d38dfa015503b32c4b591a8fd08dbad
kelmita22 <kelmita22@gmail.com>
2021-01-27 12:50:24 -0500

    actualizacion proyecyto

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,31 @@
 const cipher =
     {
-    encode: (texto, des_cif)=> {
-
-            if ( des_cif == "" || des_cif == 0|| des_cif == null) {
-            throw new TypeError("numero invalido");
-           }
-            else if (texto == "" || texto === 0 || texto == null) { 
-            throw new TypeError('Mensaje invalido');
-           }
-
-            let newText = "";
-            for (let i = 0; i < texto.length; i++){
-            let ascii = texto.charCodeAt(i);
-            if (ascii >= 65 && ascii <= 90){
-            newText += String.fromCharCode(((ascii - 65 + parseInt(des_cif)) % 26) + 65);
+              //Realizo la función dentro de la propiedad encode junto a sus parametros.
+              encode: (des_cif , texto)=> {
+              /*Para pasar los errores del test hago una instrucción usando operador lógico or usando un costructor para crear un error cuando 
+              no se puede realizar una accion. */
+              if ( des_cif == "" || des_cif == 0 || des_cif == null) {throw new TypeError("numero invalido");}
+              else if (texto == "" || texto === 0 || texto == null) {throw new TypeError("Mensaje invalido");}
+        
+              let newText = "";
+              /* 1. Para codificar utilizo un ciclo for para obtener el código de varios caracteres a la vez 
+              dentro de la cual coloco una funcion de cadenas length la que me permite obtener el número de 
+              caracteres dentro de un string*/
+              /* 2. Creo una variable para obtener un caracter en la posicion especificada del texto */
+              /* 3. En este caso sólo estoy tomando caracteres del alfabeto en mayuscula desde la ubicación 65 a 90 (A-Z) con un valor operador
+              lógico and*/
+              /*4 Para el cifrado primero convierto mis valores unicode en caracteres  */
+              /*1*/ for (let i = 0; i < texto.length; i++){
+              /*2*/ let ascii = texto.charCodeAt(i);
+              /*3*/ if (ascii >= 65 && ascii <= 90){
+              /*4*/ newText += String.fromCharCode(((ascii - 65 + parseInt(des_cif)) % 26) + 65);
            }
            }
            return newText;
            },
 
-        decode: (texto2, des_des)=>{
-          console.log(texto2, des_des);
+        decode: (des_des ,texto2)=>{
+          
            if ( des_des == "" || des_des == 0|| des_des == null) {
             throw new TypeError("numero invalido");
            }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 import cipher from './cipher.js';
-//Llamada a mis botones de cifrado y descifrado para activar los resultados / Los cambié de ser eventos onclick.
+// importamos el objeto cipher del documento cipher.js en donde almacenamos nuestras
+//Uso de selectores de dom de ID para acivar la funcion de mis botones de cifrado y descifrado.
 let btnCifrar = document.getElementById('encodeText')
 let btnDescifrar = document.getElementById('decodeText')
-// funcion para validar los desplazamientos y el ingreso de texto
+// funcion para validar el cifrado (desplazamiento,ingreso de texto y boton cifrado)
 function encodeText() {
 
-        //const form = document.Cifrar;
+        //uso elemment.name (form) para llamar a los componentes de mi formulario(document.Cifrar),textarea (newText) e input desplazamiento (des_cif));
         const form = document.Cifrar;
         let texto = form.newText.value.toUpperCase();
         let des_cif = parseInt(form.des_cif.value);
-        form.newText.value = cipher.encode(texto, des_cif);
+        form.newText.value = cipher.encode(des_cif, texto);
         }
+        //El texto ingresado lo convierto a mayus y llamo la función de cifrado con el objeto cipher encode para que me dé el cifrado en el textarea.
         btnCifrar.addEventListener('click', encodeText);
-
-  // funcion para descifrar
-  function decodeText() {
-          /*alert('funciona')*/
-          const form = document.Descifrar;
-          let des_des = parseInt(form.des_des.value);
-          /*console.log(form.des_des.value)*/
-          let resultText = form.resultText.value;
-          /*console.log(resultText)*/
-          console.log(cipher.decode(resultText, des_des))
-          form.resultText.value = cipher.decode(resultText, des_des);      
-          }
-    btnDescifrar.addEventListener('click', decodeText)
-  
+        //activo un evento (addevent) para que al dar click se muestre en el cuadro de texto el cifrado
+        
+       // funcion para validar el descifrado (desplazamiento,ingreso de texto y boton descifrado)
+function decodeText() {
+      //uso elemment.name (form) para llamar a los componentes de mi formulario(document.Desifrar),textarea (resultText) e input desplazamiento (des_des));
+        const form = document.Descifrar;
+        let des_des = parseInt(form.des_des.value);
+        let resultText = form.resultText.value.toUpperCase();
+        console.log(cipher.decode(resultText, des_des))
+        form.resultText.value = cipher.decode(des_des, resultText);      
+        }
+      //El texto ingresado lo convierto a mayus y llamo la función de cifrado con el objeto cipher.decode para que me dé el descifrado en el textarea.
+        btnDescifrar.addEventListener('click', decodeText)
+      //activo un evento (addevent) para que al dar click se muestre en el cuadro de texto el cifrado
  
